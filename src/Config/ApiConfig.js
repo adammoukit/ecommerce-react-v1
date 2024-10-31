e41f3d7351a672472c6ff21afff874e95b50b333
ADAM MOUKIT <moukitadam@gmail.com>
2024-10-31 00:07:55 +0000

    ajout du mot cle export devant la variable BASE_URL

--- src/Config/ApiConfig.js
+++ src/Config/ApiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const isProduction = import.meta.env.MODE === "production";
 
-const BASE_URL = isProduction
+export const BASE_URL = isProduction
   ? import.meta.env.VITE_API_URL // Utilisez l'URL de production
   : "http://localhost:5454"; // Utilisez localhost en développement
 // export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5454";
