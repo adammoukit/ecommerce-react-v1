4dd4f6563654c42aec6364fe0155fbe38842ff13
Moukit <moukitadam@gmail.com>
2025-07-16 22:38:56 +0000

    retouche du boutton d'ajout au panier et affichage du message lors de l'ajout au panier

--- src/customer/components/productDetails/ProductDetails.jsx
+++ src/customer/components/productDetails/ProductDetails.jsx
@@ -27,59 +27,7 @@ import BasicSelect from "../Mui/BasicSelect";
 import ProductColors from "../Utils_Components/ProductColors";
 import AliceCarousel from "react-alice-carousel";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-
-const product = {
-  name: "Basic Tee 6-Pack",
-  price: "$192",
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Men", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
-  images: [
-    {
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJkQRZ1z9PG77d5PxL1vS6J5scywL1xxC3Qg&s",
-      alt: "Two each of gray, white, and black shirts laying flat.",
-    },
-    {
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9i1EF0GnvD52UIciJlvW5bDOARyrGFmB9qQ&s",
-      alt: "Model wearing plain black basic tee.",
-    },
-    {
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-qFXtDueJs07PIpMHpLo3fRm8UQHsAzvSaw&s",
-      alt: "Model wearing plain gray basic tee.",
-    },
-    {
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1M00jVqbnasjvHXdcES3L8n1vEOah9u82OA&s",
-      alt: "Model wearing plain white basic tee.",
-    },
-    {
-      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTCHXKhcn_5jxHPIWIXh3Yl-USec9r2EDQ3Q&s",
-      alt: "Model wearing plain gray basic tee.",
-    },
-  ],
-  colors: [
-    { name: "White", class: "bg-white", selectedClass: "ring-gray-400" },
-    { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
-    { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
-  ],
-  sizes: [
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-  ],
-  description:
-    'The Basic Tee 6-Pack allows you to fully express your vibrant personality with three grayscale options. Feeling adventurous? Put on a heather gray tee. Want to be a trendsetter? Try our exclusive colorway: "Black". Need to add an extra pop of color to your outfit? Our white tee has you covered.',
-  highlights: [
-    "Hand cut and sewn locally",
-    "Dyed with our proprietary colors",
-    "Pre-washed & pre-shrunk",
-    "Ultra-soft 100% cotton",
-  ],
-  details:
-    'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
-};
-const reviews = { href: "#", average: 4, totalCount: 117 };
+import { toast } from "react-toastify";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -206,7 +154,8 @@ export default function ProductDetails() {
 
     dispatch(addItemToCart(cartData))
       .then(() => {
-        alert("Produit ajouté au panier  avec variant!");
+        // alert("Produit ajouté au panier  avec variant!");
+        toast.success("Produit ajouté au panier");
         return dispatch(getCart());
       })
       .catch((error) => alert(`Erreur: ${error.message}`));
@@ -650,7 +599,7 @@ export default function ProductDetails() {
                   fullWidth // Important pour mobile
                 >
                   <ShoppingCartIcon style={{ marginRight: "8px" }} />
-                  Ajouter
+                  Ajouter {loading && <CircularProgress size={16} />}
                 </Button>
               </div>
             </form>
